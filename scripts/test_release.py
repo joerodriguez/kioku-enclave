c4a4cbf5c4f952e83f31621717685f6976ee96e7
Joseph Rodriguez <joerodriguez@gmail.com>
2026-08-23 22:58:52 -0400

    release: repin reviewed deployment after v0.8.33 rollout

--- scripts/test_release.py
+++ scripts/test_release.py
@@ -109,7 +109,7 @@ class LocalReleaseContracts(unittest.TestCase):
             encoding="utf-8"
         )
         for evidence in (
-            "975726700360459ea887dce50ad33fd24294a176",
+            "b4b55e38497ceb56729daf957722dbf4236cac39",
             "8e12937f582abe272e51f8f1d093d41ada431d5d636792123c1fab1baabab4d5",
             "infra/enclave.tf",
             "--untracked-files=all",
